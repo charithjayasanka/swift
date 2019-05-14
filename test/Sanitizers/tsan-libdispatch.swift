d4619ae859fb78d6ce12f7283d0733f0435312d7
Julian Lettner <jlettner@apple.com>
2019-05-13 17:14:12 -0700

    [TSan] Add positive test for TSan + Dispatch on Linux
    1) Enable tests that use `import Dispatch` on Linux. Add substitution
       `%import-libdispatch` that needs to be used for all cross-platform
       tests (i.e., tests that are intended to be run on other platforms
       than Darwin) that do `import Dispatch` or enable thread sanitizer.
    
    2) Make sure as many existing Dispatch and TSan tests as possible run on
       Linux. Mark tests that would require substantial work with
       `UNSUPPORTED: OS=linux-gnu`.
    
    3) Add integration-style Swift test that shows that TSan finds a simple
       race when using `Dispatch.async` incorrectly. A more complete test
       suite for TSan's libdispatch support lives on the LLVM/compiler-rt
       side. Add and use `%deflake` substitution for deflake utility for
       this inherintly flaky test.
    
    rdar://problem/49177535

---
+++ test/Sanitizers/tsan-libdispatch.swift
@@ -0,0 +1,42 @@
+// RUN: %target-swiftc_driver %s -g -sanitize=thread %import-libdispatch -o %t_tsan-binary
+// RUN: %target-codesign %t_tsan-binary
+// RUN: %deflake env %env-TSAN_OPTIONS=abort_on_error=0 %target-run %t_tsan-binary 2>&1 | %FileCheck %s
+// REQUIRES: executable_test
+// REQUIRES: tsan_runtime
+// UNSUPPORTED: OS=tvos
+
+// FIXME: This should be covered by "tsan_runtime"; older versions of Apple OSs
+// don't support TSan.
+// UNSUPPORTED: remote_run
+
+// Test ThreadSanitizer execution end-to-end with libdispatch.
+
+import Dispatch
+
+let sem1 = DispatchSemaphore(value: 0)
+let sem2 = DispatchSemaphore(value: 0)
+
+let q = DispatchQueue(label: "q", attributes: .concurrent)
+
+var racy = 1
+
+q.async {
+  sem1.wait()
+  racy = 2
+  sem2.signal()
+}
+q.async {
+  sem1.wait()
+  racy = 3
+  sem2.signal()
+}
+
+sem1.signal()
+sem1.signal()
+sem2.wait()
+sem2.wait()
+
+print("Done!")
+
+// CHECK: ThreadSanitizer: data race
+// CHECK: Done!
